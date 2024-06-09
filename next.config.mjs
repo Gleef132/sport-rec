97a0a61ee68f361ba08a45aa9e2e02fe498708dc
Janybek <messenger-by-hack@gmail.com>
2024-06-09 15:02:06 +0600

    fix export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
